0b1200b905afcfc0b7a2affd7424c57750362587
Maks <rubelmaksum2404@gmail.com>
2025-03-11 10:59:32 +0200

    цвц

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using data_accsec_layer;
+using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DbSportShop db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new DbSportShop(ConfigurationManager.ConnectionStrings["connShop"].ConnectionString);
+
+        }
+
+        private void LoadProducts(object sender, RoutedEventArgs e)
+        {
+            datagrid.ItemsSource = db.getAll();
+        }
+
+        private void id_product_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                this.getProduct(sender,e);
+        }
+
+        private void getProduct(object sender, RoutedEventArgs e)
+        {
+            int id = int.Parse(id_product.Text);
+            List<Product> list = new List<Product>();
+            Product item = db.getOneProduct(id);
+            if (item != null)
+            {
+                list.Add(item);
+                datagrid.ItemsSource = list;
+            }
+            else
+                MessageBox.Show("Item not fount!!!","error", MessageBoxButton.OK, MessageBoxImage.Error);
+            id_product.Text = "";
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection connection;
+        SqlDataAdapter da;
+        SqlCommandBuilder bild;
+        DataSet set;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connShop"].ConnectionString);
+        }
+
+        private void Fill(object sender, RoutedEventArgs e)
+        {
+            string sql = cmd.Text;
+            da = new SqlDataAdapter(sql, connection);
+            bild = new SqlCommandBuilder(da);
+            set = new DataSet();
+
+            da.Fill(set, "MyTable");
+            datagrid.ItemsSource = set.Tables["MyTable"].DefaultView;
+
+        }
+
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            da.Update(set, "MyTable");
+        }
+
+        private void Cmb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var obj = (ComboBox)sender;
+            ComboBoxItem item = (ComboBoxItem)obj.Items[obj.SelectedIndex];
+         
+            string sql = "select * from " + item.Content.ToString();
+         
+            cmd.Text = sql;
+         
+        }
+    }
+}
